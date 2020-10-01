18c9eb511c1bc760de20bd6710d81ae59115f350
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2020-09-30 19:15:59 -0500

    Ejercicio 5 en parejas, clase Cliente lista, falta diseñar

---
+++ EjerciciosU2ClienteServidor/Ejercicio5_ClientePeliculas/DatosPelicula.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5_ClientePeliculas
+{
+    public enum Sala { A1 =0, A2=1, B1=2, B2=3};
+    public enum Clasificacion { A=0, B15=1, C=2, R=3};
+    public enum Idioma { Español=0, Subtitulada=1};
+    public class DatosPelicula
+    {
+        public string Hora { get; set; }
+        public string Nombre { get; set; }
+        public Sala Sala { get; set; }
+        public Clasificacion Clasificacion { get; set; }
+        public Idioma Idioma { get; set; }
+    }
+}
---
+++ EjerciciosU2ClienteServidor/Ejercicio5_ClientePeliculas/PeliculasClient.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5_ClientePeliculas
+{
+    public class PeliculasClient
+    {
+        HttpClient cliente = new HttpClient();
+
+        public PeliculasClient()
+        {
+            cliente.BaseAddress = new Uri("http://*:8080/Ejercicio5/");
+        }
+
+        public async void Agregar(DatosPelicula p)
+        {
+            var json = JsonConvert.SerializeObject(p);
+            var result = await cliente.PostAsync("Tablero", new StringContent(json, Encoding.UTF8, "application/json"));
+            result.EnsureSuccessStatusCode();
+        }
+
+        public async void Eliminar(DatosPelicula p)
+        {
+            var json = JsonConvert.SerializeObject(p);
+            HttpRequestMessage msj = new HttpRequestMessage(HttpMethod.Delete, "Tablero");
+            msj.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await cliente.SendAsync(msj);
+
+            result.EnsureSuccessStatusCode();
+        }
+
+        public async void Editar(DatosPelicula p)
+        {
+            var json = JsonConvert.SerializeObject(p);
+            var result = await cliente.PutAsync("Tablero", new StringContent(json, Encoding.UTF8, "application/json"));
+            result.EnsureSuccessStatusCode();
+        }
+
+        public delegate void cambio();
+        public event cambio AlHaberCambios;
+        public IEnumerable<DatosPelicula> Model { get; set; }
+        public async void Get()
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("http://*:8080/Ejercicio5/Tablero");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                Model = JsonConvert.DeserializeObject<IEnumerable<DatosPelicula>>(jsonString);
+                AlHaberCambios?.Invoke();
+            }
+        }
+
+    }
+}
